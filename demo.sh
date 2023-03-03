2fc99bd7025d7a16972578f3f30ecad4b602aa9e
Viral Kanzariya <viral.kanzariya@hiddenbrains.in>
2023-03-03 19:56:37 +0530

    Add the sh file

--- demo.sh
+++ demo.sh
@@ -1,1 +1,11 @@
-echo "Hello World"
+bash
+#!/bin/bash
+
+# Get the commit SHA of the previous commit
+prev_commit=$(git rev-parse HEAD^)
+
+# Get the changed lines in the file
+changed_lines=$(git diff --unified=0 $prev_commit HEAD $1 | grep -E "^\+")
+
+# Print the changed lines
+echo "$changed_lines"
